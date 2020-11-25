0b3202badcf8e64b4bfc7ec0bc503d412c2f6753
Robert Floor <robertfloor@MacBook-Pro-van-Robert.local>
2020-11-25 16:46:36 +0100

    changed travis file for Docker test run

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+
 docker build -t rflf/multi-client -f ./client/Dockerfile ./client
 docker build -t rflf/multi-server -f ./server/Dockerfile ./server
 docker build -t rflf/multi-worker -f ./worker/Dockerfile ./worker
