2fb4b6b289229b46939dc5b50e723be99986e435
Robert Floor <robertfloor@MacBook-Pro-van-Robert.local>
2020-11-25 17:01:12 +0100

    changed travis file for Docker test run 3

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 
-docker build -t rflf/multi-client -f ./client/Dockerfile ./client
-docker build -t rflf/multi-server -f ./server/Dockerfile ./server
-docker build -t rflf/multi-worker -f ./worker/Dockerfile ./worker
-
 
 
 kubectl apply -f k8s
 
-kubectl rollout restart RESOURCE
